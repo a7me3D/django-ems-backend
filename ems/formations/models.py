7be3818d10e8b57230e6fa1a53bf9462ca8132a1
a7me3D <ahmedjbeli54@yahoo.fr>
2021-05-05 00:47:38 +0100

    Formation model

--- ems/formations/models.py
+++ ems/formations/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Formation(models.Model):
+    titre = models.CharField(max_length=200, blank=False, default="")
+    genre = models.CharField(max_length=200, blank=False, default="")
+    jour = models.DateField(
+        blank=True, default=timezone.now)
+    formateur = models.CharField(max_length=200, blank=False, default="")
+    duree = models.IntegerField(default=1)
+    lieu = models.CharField(max_length=200, blank=False, default="")
+    sujet = models.CharField(max_length=300, blank=False, default="")
