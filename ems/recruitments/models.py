3e5b902b07938b3e4fe6b23b29b99b5485b398bd
a7me3D <ahmedjbeli54@yahoo.fr>
2021-05-05 21:38:16 +0100

    setup recruitment model

--- ems/recruitments/models.py
+++ ems/recruitments/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+from django.utils import timezone
+
+
+class Recruitment(models.Model):
+    POSTE = [
+        ("FT", 'Temps plein'),
+        ("PT", 'Temp partiel'),
+        ("ST", 'Sous-traitant'),
+        ("S", 'Stagiaire'),
+    ]
+
+    direction = models.CharField(max_length=100, blank=False)
+    responsable = models.CharField(max_length=100, blank=False)
+    date_demande = models.DateField(default=timezone.now)
+    poste = models.CharField(max_length=100, blank=False)
+    poste_type = models.CharField(
+        max_length=100, choices=POSTE, default=POSTE[0][0])
+    poste_description = models.TextField()
+    motivation = models.TextField()
+    exp_requise = models.TextField()
+    formation_requise = models.TextField()
