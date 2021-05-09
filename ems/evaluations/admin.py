1d409604dfc6846e86dd15259df76e6a44bb3a65
a7me3D <ahmedjbeli54@yahoo.fr>
2021-05-09 01:09:28 +0100

    register Evaluation and Criteria to admin site

--- ems/evaluations/admin.py
+++ ems/evaluations/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Evaluation, Criteria
 # Register your models here.
 
-# admin.site.register(Evaluation)
-# admin.site.register(Criteria)
+admin.site.register(Evaluation)
+admin.site.register(Criteria)
