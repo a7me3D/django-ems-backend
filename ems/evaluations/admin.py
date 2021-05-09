0d4865ba6cecc83a2d0fd6dbebad1ccc32606dbc
a7me3D <ahmedjbeli54@yahoo.fr>
2021-05-09 01:59:44 +0100

    register CriteriaType

--- ems/evaluations/admin.py
+++ ems/evaluations/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Evaluation, Criteria
+from .models import Evaluation, Criteria, CriteriaType
 # Register your models here.
 
 admin.site.register(Evaluation)
 admin.site.register(Criteria)
+admin.site.register(CriteriaType)
