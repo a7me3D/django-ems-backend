a2559296a373288d1793d313df0a2a24f5560709
a7me3D <ahmedjbeli54@yahoo.fr>
2021-05-11 00:13:25 +0100

    add List + Detail + Delete View

--- ems/evaluations/urls.py
+++ ems/evaluations/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import EvaluationCreationView
+from .views import EvaluationCreationView, EvaluationDetailView, EvaluationListView, EvaluationDeleteView
 
 app_name = "evaluations"
 
 urlpatterns = [
-    path('create', EvaluationCreationView.as_view(), name="evaluation-create"),
+    path('', EvaluationListView.as_view(),
+         name="evaluations-view"),
+    path('<int:pk>/evaluate', EvaluationCreationView.as_view(),
+         name="evaluation-create"),
+    path('me', EvaluationDetailView.as_view(),
+         name="evaluation-detail"),
+    path('me', EvaluationDetailView.as_view(),
+         name="evaluation-detail"),
+
+    path('<int:pk>/delete', EvaluationDeleteView.as_view(),
+         name="evaluation-delete"),
 ]
--- ems/evaluations/views.py
+++ ems/evaluations/views.py
@@ -1,20 +1,27 @@
-from django.shortcuts import render, HttpResponse
 from django.views import generic
+from django.shortcuts import reverse
+from users.models import Employee
 from .models import Evaluation, Criteria, CriteriaType
-from .forms import CriteriaForm, CriteriaFormSet
+from .forms import CriteriaFormSet
 
 
 class EvaluationCreationView(generic.CreateView):
     model = Evaluation
     template_name = "evaluation_create.html"
     fields = ["start_period", "end_period"]
-    success_url = "/"
+
+    def get_success_url(self):
+        return reverse("evaluations:evaluations-view")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        criteria_types = CriteriaType.objects.all()
+        # criteria_types = CriteriaType.objects.all()
         initial_types = [{"title": Ctype}
                          for Ctype in CriteriaType.objects.all()]
+
+        employee_id = self.kwargs.get("pk")
+        employee = Employee.objects.get(pk=employee_id)
+        context['employee'] = employee
 
         formset = CriteriaFormSet(
             initial=initial_types, queryset=Criteria.objects.none())
@@ -24,7 +31,7 @@ class EvaluationCreationView(generic.CreateView):
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.employee = self.request.user
-        self.object.evaluator = self.request.user
+        self.object.evaluator = Employee.objects.get(pk=self.kwargs.get("pk"))
         self.object.save()
 
         formset = CriteriaFormSet(self.request.POST)
@@ -32,7 +39,33 @@ class EvaluationCreationView(generic.CreateView):
             if cform.is_valid():
                 cformid = cform.save()
                 self.object.criteria.add(cformid)
-            else:
-                print('nope')
 
         return super(EvaluationCreationView, self).form_valid(form)
+
+
+class EvaluationDetailView(generic.DetailView):
+    model = Evaluation
+    template_name = 'evaluation_detail.html'
+    context_object_name = "evaluation"
+
+    def get_object(self):
+        evaluation = Evaluation.objects.filter(
+            employee=self.request.user.id).last()
+        return evaluation
+
+
+class EvaluationListView(generic.ListView):
+    model = Evaluation
+    template_name = 'evaluations_view.html'
+    context_object_name = "evaluations"
+
+
+class EvaluationDeleteView(generic.DeleteView):
+    template_name = "evaluation_delete.html"
+    queryset = Evaluation.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse("evaluations:evaluations-view")
