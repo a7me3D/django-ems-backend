ee0693a8ee5973313ef282a418dd9112a9e8ae23
a7me3D <ahmedjbeli54@yahoo.fr>
2021-05-11 03:32:13 +0100

    Finance CRUD

--- ems/finance/urls.py
+++ ems/finance/urls.py
@@ -3,6 +3,15 @@ from .views import *
 app_name = "finances"
 
 urlpatterns = [
-
-
+    path('', FinanceListView.as_view(), name="finance-view"),
+    path('create', FinanceCreationView.as_view(), name="finance-create"),
+    path('<int:pk>', FinanceDetailView.as_view(), name="finance-detail"),
+    path('<int:pk>/update/', FinanceUpdateView.as_view(),
+         name='finance-update'),
+    path('<int:pk>/delete/', FinanceDeleteView.as_view(),
+         name='finance-delete'),
+    path('<int:pk>/accept/', FinanceAcceptView.as_view(),
+         name='finance-accept'),
+    path('<int:pk>/reject', FinanceRejectView.as_view(),
+         name='finance-reject')
 ]
--- ems/finance/views.py
+++ ems/finance/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import reverse, HttpResponseRedirect
+from django.views import generic
+from .models import FinanceDoc as Finance
 
-# Create your views here.
+
+class FinanceListView(generic.ListView):
+    model = Finance
+    template_name = 'finance_view.html'
+    context_object_name = "finances"
+
+
+class FinanceCreationView(generic.CreateView):
+    model = Finance
+    template_name = "finance_create.html"
+    fields = ["doc_type", "montant", "motif",
+              "nb_echeances", "avis_chef", "avis_rf", ]
+
+    def get_success_url(self):
+        return reverse("finances:finance-view")
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.employee = self.request.user
+        self.object.save()
+        return super(FinanceCreationView, self).form_valid(form)
+
+
+class FinanceDetailView(generic.DetailView):
+    model = Finance
+    template_name = 'finance_detail.html'
+    context_object_name = "finance"
+
+
+class FinanceUpdateView(generic.UpdateView):
+    model = Finance
+    template_name = "finance_update.html"
+    fields = ["doc_type", "montant", "motif",
+              "nb_echeances", "avis_chef", "avis_rf", ]
+
+    def get_success_url(self):
+        return reverse("finances:finance-view")
+
+
+class FinanceDeleteView(generic.DeleteView):
+    template_name = "formation_delete.html"
+    queryset = Finance.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse("finances:finance-view")
+
+
+class FinanceAcceptView(generic.View):
+    def get(self, request, *args, **kwargs):
+        doc = Finance.objects.get(pk=self.kwargs.get("pk"))
+        doc.status = dict(Finance.STATUS_TYPE).get("A")
+        doc.save()
+        return HttpResponseRedirect(reverse("finances:finance-view"))
+
+
+class FinanceRejectView(generic.View):
+    def get(self, request, *args, **kwargs):
+        doc = Finance.objects.get(pk=self.kwargs.get("pk"))
+        doc.status = dict(Finance.STATUS_TYPE).get("R")
+        doc.save()
+        return HttpResponseRedirect(reverse("finances:finance-view"))
