221006696e5c3cb8ab90a8c1024972707206fa45
a7me3D <ahmedjbeli54@yahoo.fr>
2021-05-12 03:08:09 +0100

    setup models

--- ems/conges/models.py
+++ ems/conges/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils import timezone
+from users.models import Employee
 
-# Create your models here.
+
+def one_day_hence():
+    return timezone.now() + timezone.timedelta(days=1)
+
+
+class Conge(models.Model):
+    CONGE_TYPE = [
+        ("A", "annuel"),
+        ("E", "exceptionnel"),
+        ("M", "maladie"),
+        ("R", "recuperation"),
+    ]
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    demand_date = models.DateField(default=timezone.now)
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField(default=one_day_hence)
+    conge_type = models.CharField(
+        max_length=1, choices=CONGE_TYPE, default=CONGE_TYPE[0][0])
+
+    @property
+    def days(self):
+        return self.end_date - self.start_date
+
+
+class CongeTitle(models.Model):
+    conge = models.ForeignKey(Conge, on_delete=models.CASCADE)
+    interim = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    solde = models.IntegerField(default=30)
+
+    @property
+    def rest(self):
+        return self.solde - self.conge.days
+
+    @property
+    def days(self):
+        return self.conge.days
