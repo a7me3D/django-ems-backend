2fe93ef3018d722cd1f486998957a9e031b69e85
a7me3D <ahmedjbeli54@yahoo.fr>
2021-05-13 05:02:36 +0100

    add default  login required

--- ems/users/decorators.py
+++ ems/users/decorators.py
@@ -1,8 +1,14 @@
+from django.shortcuts import HttpResponse
+
+
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
-            if request.user.poste in allowed_roles:
-                return view_func(request, *args, **kwargs)
+            if request.user.is_authenticated:
+                if request.user.poste in allowed_roles:
+                    return view_func(request, *args, **kwargs)
+                else:
+                    return HttpResponse('You are not authorized to view this page')
             else:
                 return HttpResponse('You are not authorized to view this page')
         return wrapper_func
