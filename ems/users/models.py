e96ed0c12a498b3731d999133d1f3d82618445d3
a7me3D <ahmedjbeli54@yahoo.fr>
2021-05-13 04:25:18 +0100

    add evaluated property

--- ems/users/models.py
+++ ems/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from evaluations.models import Evaluation
 
 
 class Employee(AbstractUser):
@@ -31,3 +32,7 @@ class Employee(AbstractUser):
 
     username = None
     REQUIRED_FIELDS = []
+
+    @property
+    def evaluated(self):
+        return Evaluation.objects.filter(employee__id=self.id).count()
