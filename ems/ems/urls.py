1358adff0b37153c4fbcef7e3cc311bd3d204202
a7me3D <ahmedjbeli54@yahoo.fr>
2021-05-13 04:22:28 +0100

    remove app_name

--- ems/ems/urls.py
+++ ems/ems/urls.py
@@ -3,9 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 from users.views import HomeView
 
-app_name = "ems"
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', HomeView.as_view(), name="home"),
